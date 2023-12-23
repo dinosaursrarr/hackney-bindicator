6155cde53241f1505fb944b6010873694d0b72e6
dinosaursrarr <tom+github@chamberlaincurtis.com>
2023-12-22 18:17:58 -0800

    Fourth API call: get workflow ID

---
+++ client/workflow_id.go
@@ -0,0 +1,87 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func (c BinsClient) GetBinWorkflowId(binId, token string) (string, error) {
+	reqBody := []byte(`{
+        "type": "Query",
+        "aqs": {
+            "properties": {
+                "dodiCode": "designs_scheduleCode_5f8d5eac8dae040066c53c8c",
+                "attributes": [
+                    "attributes_scheduleCodeWorkflowID_5f8dbfdce27d98006789b4ec",
+                ],
+            },
+            "children": [
+                {
+                    "type": "Equals",
+                    "children": [
+                        {
+                            "type": "Attribute",
+                            "properties": {
+                                "attributeCode": "attributes_assetGroupsAssets",
+                                "path": "root^Template:attributes_wasteRoundsScheduleCode_5f8de8de8dae040066c59dae.Template:attributes_projectsTasks^Live:attributes_tasksAssignableTasks<designInterfaces_assetGroups>",
+                                "value": [],
+                            },
+                        },
+                        {
+                            "type": "AlloyId",
+                            "properties": {
+                                "value": [
+                                    "` + binId + `",
+                                ],
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+    }`)
+	target := c.ApiHost.JoinPath(queryUrl).String()
+	req, err := http.NewRequest(http.MethodPost, target, bytes.NewBuffer(reqBody))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Add("Authorization", fmt.Sprint("Bearer ", token))
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := c.HttpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	if resp.StatusCode != 200 {
+		return "", fmt.Errorf("Status code %v fetching list of bins", resp.StatusCode)
+	}
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	type result struct {
+		Results []struct {
+			Attributes []struct {
+				AttributeCode string `json:"attributeCode"`
+				Value         string `json:"value"`
+			} `json:"attributes"`
+		} `json:"results"`
+	}
+
+	var data result
+	json.Unmarshal(respBody, &data)
+	for _, res := range data.Results {
+		for _, attribute := range res.Attributes {
+			if attribute.AttributeCode != "attributes_scheduleCodeWorkflowID_5f8dbfdce27d98006789b4ec" {
+				continue
+			}
+			return attribute.Value, nil
+		}
+	}
+	return "", errors.New("Workflow ID not found")
+}
---
+++ client/workflow_id_test.go
@@ -0,0 +1,157 @@
+package client_test
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"testing/iotest"
+
+	"github.com/dinosaursrarr/hackney-bindicator/client"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBadWorkflowIdUrl(t *testing.T) {
+	badUrl, _ := url.Parse("ftp://foo.bar")
+	client := client.BinsClient{http.Client{}, nil, badUrl, nil}
+
+	res, err := client.GetBinWorkflowId(BinId, Token)
+
+	assert.Empty(t, res)
+	assert.Contains(t, err.Error(), "unsupported protocol scheme")
+}
+
+func TestSetAccessTokenGettingWorkflowId(t *testing.T) {
+	apiSvr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Contains(t, r.Header["Authorization"], "Bearer "+Token)
+	}))
+	defer apiSvr.Close()
+	apiUrl, _ := url.Parse(apiSvr.URL)
+	client := client.BinsClient{http.Client{}, nil, apiUrl, nil}
+
+	client.GetBinWorkflowId(BinId, Token)
+}
+
+func TestHttpErrorGettingWorkflowId(t *testing.T) {
+	apiSvr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	defer apiSvr.Close()
+	apiUrl, _ := url.Parse(apiSvr.URL)
+	httpClient := http.Client{
+		Transport: fakeRoundTripper{
+			Fn: func(req *http.Request) (*http.Response, error) {
+				return nil, errors.New("foo")
+			},
+		},
+	}
+	client := client.BinsClient{httpClient, nil, apiUrl, nil}
+
+	res, err := client.GetBinWorkflowId(BinId, Token)
+
+	assert.Empty(t, res)
+	assert.Contains(t, err.Error(), "foo")
+}
+
+func TestBadStatusCodeGettingWorkflowId(t *testing.T) {
+	apiSvr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "nope", http.StatusTeapot)
+	}))
+	defer apiSvr.Close()
+	apiUrl, _ := url.Parse(apiSvr.URL)
+	client := client.BinsClient{http.Client{}, nil, apiUrl, nil}
+
+	res, err := client.GetBinWorkflowId(BinId, Token)
+
+	assert.Empty(t, res)
+	assert.Contains(t, err.Error(), "Status code 418")
+}
+
+func TestErrorReadingWorkflowId(t *testing.T) {
+	apiSvr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	defer apiSvr.Close()
+	apiUrl, _ := url.Parse(apiSvr.URL)
+	httpClient := http.Client{
+		Transport: fakeRoundTripper{
+			Fn: func(req *http.Request) (*http.Response, error) {
+				return &http.Response{
+					StatusCode: 200,
+					Body:       ioutil.NopCloser(iotest.ErrReader(errors.New("nope"))),
+				}, nil
+			},
+		},
+	}
+	client := client.BinsClient{httpClient, nil, apiUrl, nil}
+
+	res, err := client.GetBinWorkflowId(BinId, Token)
+
+	assert.Empty(t, res)
+	assert.Contains(t, err.Error(), "nope")
+}
+
+func TestWorkflowIdNotFound(t *testing.T) {
+	apiSvr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	defer apiSvr.Close()
+	apiUrl, _ := url.Parse(apiSvr.URL)
+	client := client.BinsClient{http.Client{}, nil, apiUrl, nil}
+
+	res, err := client.GetBinWorkflowId(BinId, Token)
+
+	assert.Empty(t, res)
+	assert.Contains(t, err.Error(), "Workflow ID not found")
+}
+
+func TestEmptyWorkflowIdFound(t *testing.T) {
+	apiSvr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, `
+			{
+				"results": [
+					{
+						"attributes": [
+							{
+								"attributeCode": "attributes_scheduleCodeWorkflowID_5f8dbfdce27d98006789b4ec",
+								"value": ""
+							}
+						]
+					}
+				]
+			}
+		`)
+	}))
+	defer apiSvr.Close()
+	apiUrl, _ := url.Parse(apiSvr.URL)
+	client := client.BinsClient{http.Client{}, nil, apiUrl, nil}
+
+	res, err := client.GetBinWorkflowId(BinId, Token)
+
+	assert.Empty(t, res)
+	assert.Nil(t, err)
+}
+
+func TestSuccessWorkflowId(t *testing.T) {
+	apiSvr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, `
+			{
+				"results": [
+					{
+						"attributes": [
+							{
+								"attributeCode": "attributes_scheduleCodeWorkflowID_5f8dbfdce27d98006789b4ec",
+								"value": "foo"
+							}
+						]
+					}
+				]
+			}
+		`)
+	}))
+	defer apiSvr.Close()
+	apiUrl, _ := url.Parse(apiSvr.URL)
+	client := client.BinsClient{http.Client{}, nil, apiUrl, nil}
+
+	res, err := client.GetBinWorkflowId(BinId, Token)
+
+	assert.Equal(t, res, "foo")
+	assert.Nil(t, err)
+}
