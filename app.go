f1e142b80b960f24d5a68769e042adbc48f65b8f
dinosaursrarr <tom+github@chamberlaincurtis.com>
2025-12-12 08:33:15 +0000

    Simplify readme embedding

--- app.go
+++ app.go
@@ -18,9 +18,6 @@ import (
 	"github.com/jonboulle/clockwork"
 )
 
-//go:embed README.md
-var readme []byte
-
 //go:embed static/*
 var static embed.FS
 
@@ -51,6 +48,10 @@ func main() {
 	addressHandler := handler.AddressHandler{
 		Client: binsClient,
 		Cache:  cache,
+	}
+	readme, err := static.ReadFile("static/README.md")
+	if err != nil {
+		log.Fatal("Could not load readme")
 	}
 	readmeHandler := handler.MarkdownHandler{
 		Markdown: readme,
